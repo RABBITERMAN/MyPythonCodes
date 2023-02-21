1a2559e65780302be7c254fad4702da10139bf37
mo from citydi <mo.nasseri@gmail.com>
2023-02-21 18:05:44 +0330

    string comit

--- example07.py
+++ example07.py
@@ -22,6 +22,18 @@ a = -3.6
 print(type(a)) 
 a = 3.14j
 print(type(a)) 
+
 #Strings
 
+name = "welcome to Devops class"
+print (name * 2)
+print (name[::])
+print (name[:])
+print (name[0:6+1:])
+print (name[:7:])
+print (name[11:17:])
+print (name[11:17:][::-1])
+print (name[16:10:-1])
+print ((name[16:10:-1]) + "hiiiii" )
+print (((name[16:10:-1]) + "hiiiii" ) [::-1] )
 
